4d229aaf72d739bbe78ea6f7ebd6a3dc607d08e0
Robyn <oddert@gmail.com>
2019-04-24 00:05:07 +0100

    minor parameter changes

--- stl_loader.js
+++ stl_loader.js
@@ -68,6 +68,23 @@ loader.load('JetPack_Bunny.stl', function ( geometry ) {
   // console.log('wheres my bunny i was expecting a bunny')
   render()
 });
+loader.load('JetPack_Bunny.stl', function ( geometry ) {
+  var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
+	var mesh = new THREE.Mesh( geometry, material );
+
+	mesh.position.set( 0, - 0.25, 0.6 );
+	// mesh.rotation.set( 0, - Math.PI / 2, 0 );
+	mesh.rotation.set( -Math.PI/2, 0, Math.PI / 2 - .5 );
+	mesh.scale.set( 0.5, 0.5, 0.5 );
+
+	mesh.castShadow = true;
+	mesh.receiveShadow = true;
+
+  console.log('jet pac bunny init')
+	scene.add( mesh );
+  // console.log('wheres my bunny i was expecting a bunny')
+  render()
+});
 
 
 
